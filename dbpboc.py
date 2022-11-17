51fdc68f380a36f2e69d7f6b271fc73368d786bc
Vincent Yang <yf2005@gmail.com>
2022-11-17 08:16:09 +0800

    initial commit

---
+++ dbpboc.py
@@ -0,0 +1,579 @@
+import glob
+import os
+import time
+import random
+import pandas as pd
+import streamlit as st
+from snapshot import get_chrome_driver
+from selenium.webdriver.common.by import By
+from utils import get_now
+from urllib.parse import unquote
+
+
+penpboc = "pboc"
+temppath = "pboc/temp"
+
+# choose orgname index
+org2name = {
+    "天津": "tianjin",
+    "重庆": "chongqing",
+    "上海": "shanghai",
+    "兰州": "lanzhou",
+    "拉萨": "lasa",
+    "西宁": "xining",
+    "乌鲁木齐": "wulumuqi",
+    "南宁": "nanning",
+    "贵阳": "guiyang",
+    "福州": "fuzhou",
+    "成都": "chengdu",
+    "呼和浩特": "huhehaote",
+    "郑州": "zhengzhou",
+    "北京": "beijing",
+    "合肥": "hefei",
+    "厦门": "xiamen",
+    "海口": "haikou",
+    "大连": "dalian",
+    "广州": "guangzhou",
+    "太原": "taiyuan",
+    "石家庄": "shijiazhuang",
+    "总部": "zongbu",
+    "昆明": "kunming",
+    "青岛": "qingdao",
+    "沈阳": "shenyang",
+    "长沙": "changsha",
+    "深圳": "shenzhen",
+    "武汉": "wuhan",
+    "银川": "yinchuan",
+    "西安": "xian",
+    "哈尔滨": "haerbin",
+    "长春": "changchun",
+    "宁波": "ningbo",
+    "杭州": "hangzhou",
+    "南京": "nanjing",
+    "济南": "jinan",
+    "南昌": "nanchang",
+}
+
+org2url = {
+    "天津": "tianjin",
+    "重庆": "chongqing",
+    "上海": "shanghai",
+    "兰州": "lanzhou",
+    "拉萨": "lasa",
+    "西宁": "xining",
+    "乌鲁木齐": "wulumuqi",
+    "南宁": "nanning",
+    "贵阳": "guiyang",
+    "福州": "fuzhou",
+    "成都": "chengdu",
+    "呼和浩特": "huhehaote",
+    "郑州": "zhengzhou",
+    "北京": "beijing",
+    "合肥": "hefei",
+    "厦门": "xiamen",
+    "海口": "haikou",
+    "大连": "dalian",
+    "广州": "guangzhou",
+    "太原": "taiyuan",
+    "石家庄": "shijiazhuang",
+    "总部": "zongbu",
+    "昆明": "kunming",
+    "青岛": "http://qingdao.pbc.gov.cn/qingdao/126166/126184/126191/16720/index",
+    "沈阳": "shenyang",
+    "长沙": "changsha",
+    "深圳": "shenzhen",
+    "武汉": "wuhan",
+    "银川": "yinchuan",
+    "西安": "xian",
+    "哈尔滨": "haerbin",
+    "长春": "changchun",
+    "宁波": "ningbo",
+    "杭州": "hangzhou",
+    "南京": "nanjing",
+    "济南": "jinan",
+    "南昌": "nanchang",
+}
+
+
+def get_csvdf(penfolder, beginwith):
+    files2 = glob.glob(penfolder + "**/" + beginwith + "*.csv", recursive=True)
+    dflist = []
+    # filelist = []
+    for filepath in files2:
+        pendf = pd.read_csv(filepath)
+        dflist.append(pendf)
+        # filelist.append(filename)
+    if len(dflist) > 0:
+        df = pd.concat(dflist)
+        df.reset_index(drop=True, inplace=True)
+    else:
+        df = pd.DataFrame()
+    return df
+
+
+def searchpboc(df, title_text, location_text):
+
+    col = [
+        "企业名称",
+        "处罚决定书文号",
+        "违法行为类型",
+        "行政处罚内容",
+        "作出行政处罚决定机关名称",
+        "作出行政处罚决定日期",
+        "区域",
+        "文件",
+        # "来源",
+    ]
+
+    searchdf = df[
+        (df["企业名称"].str.contains(title_text)) & (df["区域"].str.contains(location_text))
+    ][col]
+
+    # reset index
+    searchdf.reset_index(drop=True, inplace=True)
+
+    return searchdf
+
+
+# summary of pboc
+def display_summary():
+    # get old sumeventdf
+    oldsum2 = get_pbocdetail("")
+    # get length of old eventdf
+    oldlen2 = len(oldsum2)
+    # get min and max date of old eventdf
+    min_date2 = oldsum2["作出行政处罚决定日期"].min()
+    max_date2 = oldsum2["作出行政处罚决定日期"].max()
+    # use metric
+    col1, col2 = st.columns([1, 3])
+    with col1:
+        st.metric("案例总数", oldlen2)
+    with col2:
+        st.metric("案例日期范围", f"{min_date2} - {max_date2}")
+
+    # sum max,min date and size by org
+    sumdf2 = (
+        oldsum2.groupby("区域")["作出行政处罚决定日期"].agg(["max", "min", "count"]).reset_index()
+    )
+    sumdf2.columns = ["区域", "最近发文日期", "最早发文日期", "案例总数"]
+    # sort by date
+    sumdf2.sort_values(by=["最近发文日期"], ascending=False, inplace=True)
+    # reset index
+    sumdf2.reset_index(drop=True, inplace=True)
+    # display
+    st.markdown("#### 按区域统计")
+    st.table(sumdf2)
+
+    return sumdf2
+
+
+def get_pbocsum(orgname):
+    org_name_index = org2name[orgname]
+    beginwith = "pbocsum" + org_name_index
+    pendf = get_csvdf(penpboc, beginwith)
+    # if not empty
+    if len(pendf) > 0:
+        # format date
+        pendf["发布日期"] = pd.to_datetime(pendf["date"]).dt.date
+    return pendf
+
+
+def get_pbocdetail(orgname):
+    if orgname == "":
+        org_name_index = ""
+    else:
+        org_name_index = org2name[orgname]
+
+    beginwith = "pbocdtl" + org_name_index
+    d0 = get_csvdf(penpboc, beginwith)
+    # reset index
+    # d1 = d0[["title", "subtitle", "date", "doc", "id"]].reset_index(drop=True)
+    # format date
+    # d1["date"] = pd.to_datetime(d1["date"]).dt.date
+    # update column name
+    # d1.columns = ["标题", "文号", "发布日期", "内容", "id"]
+    # if not empty
+    if len(d0) > 0:
+        # format date
+        d0["发布日期"] = pd.to_datetime(d0["date"]).dt.date
+    # fillna
+    d0.fillna("", inplace=True)
+    return d0
+
+
+def display_suminfo(df):
+    # get length of old eventdf
+    oldlen = len(df)
+    if oldlen > 0:
+        # get min and max date of old eventdf
+        min_date = df["发布日期"].min()
+        max_date = df["发布日期"].max()
+    else:
+        min_date = ""
+        max_date = ""
+    # use metric for length and date
+    col1, col2 = st.columns([1, 3])
+    # col1.metric("案例总数", oldlen)
+    col1.write(f"案例总数：{oldlen}")
+    # col2.metric("日期范围", f"{min_date} - {max_date}")
+    col2.write(f"日期范围：{min_date} - {max_date}")
+
+
+def display_pbocsum():
+    for org_name in org2name.keys():
+        st.markdown("#### " + org_name)
+        st.markdown("列表")
+        oldsum = get_pbocsum(org_name)
+        display_suminfo(oldsum)
+        st.markdown("详情")
+        dtl = get_pbocdetail(org_name)
+        dtl1 = dtl.drop_duplicates(subset=["name", "date", "link"])
+        display_suminfo(dtl1)
+
+
+# get sumeventdf in page number range
+def get_sumeventdf(orgname, start, end):
+    org_name_index = org2name[orgname]
+    browser = get_chrome_driver()
+
+    baseurl = org2url[orgname]
+
+    resultls = []
+    errorls = []
+    count = 0
+    for i in range(start, end + 1):
+        st.info("page: " + str(i))
+        st.info(str(count) + " begin")
+        url = baseurl + str(i) + ".html"
+        st.info("url:" + url)
+        try:
+            browser.implicitly_wait(3)
+            browser.get(url)
+            ls1 = browser.find_elements(By.XPATH, '//td[@class="hei12jj"]')
+            namels = []
+            datels = []
+            sumls = []
+            total = len(ls1) // 3
+            for i in range(total):
+                #     print(ls1[i].text)
+                namels.append(ls1[i * 3].text)
+                datels.append(ls1[i * 3 + 1].text)
+                sumls.append(ls1[i * 3 + 2].text)
+
+            ls2 = browser.find_elements(By.XPATH, '//font[@class="hei12"]/a')
+            linkls = []
+            for link in ls2:
+                linkls.append(link.get_attribute("href"))
+            df = pd.DataFrame(
+                {"name": namels, "date": datels, "link": linkls, "sum": sumls}
+            )
+            resultls.append(df)
+        except Exception as e:
+            st.error("error!: " + str(e))
+            errorls.append(url)
+
+        mod = (i + 1) % 2
+        if mod == 0 and count > 0:
+            tempdf = pd.concat(resultls)
+            savename = "tempsum-" + org_name_index + str(count + 1)
+            savedf(tempdf, savename)
+
+        wait = random.randint(2, 20)
+        time.sleep(wait)
+        st.info("finish: " + str(count))
+        count += 1
+
+    sumdf = pd.concat(resultls)
+    savecsv = "tempsumall" + org_name_index + str(count)
+    savedf(sumdf, savecsv)
+    return sumdf
+
+
+def savedf(df, basename):
+    savename = basename + ".csv"
+    savepath = os.path.join(penpboc, savename)
+    df.to_csv(savepath)
+
+
+# update sumeventdf
+def update_sumeventdf(currentsum, orgname):
+    org_name_index = org2name[orgname]
+    # get detail
+    oldsum = get_pbocdetail(orgname)
+    if oldsum.empty:
+        oldidls = []
+    else:
+        oldidls = oldsum["link"].tolist()
+    currentidls = currentsum["link"].tolist()
+    # print('oldidls:',oldidls)
+    # print('currentidls:', currentidls)
+    # get current idls not in oldidls
+    newidls = [x for x in currentidls if x not in oldidls]
+    # print('newidls:', newidls)
+    # newidls=list(set(currentidls)-set(oldidls))
+    newdf = currentsum[currentsum["link"].isin(newidls)]
+    # if newdf is not empty, save it
+    if newdf.empty is False:
+        newdf.reset_index(drop=True, inplace=True)
+        nowstr = get_now()
+        savename = "pbocsum" + org_name_index + nowstr
+        savedf(newdf, savename)
+        # save to update dtl list
+        toupdname = "pboctoupd" + org_name_index
+        savedf(newdf, toupdname)
+    return newdf
+
+
+def get_pboctoupd(orgname):
+    org_name_index = org2name[orgname]
+    beginwith = "pboctoupd" + org_name_index
+    pendf = get_csvdf(penpboc, beginwith)
+    return pendf
+
+
+# update toupd
+def update_toupd(orgname):
+    org_name_index = org2name[orgname]
+    # get sumeventdf
+    currentsum = get_pbocsum(orgname)
+    # get detail
+    oldsum = get_pbocdetail(orgname)
+    if oldsum.empty:
+        oldidls = []
+    else:
+        oldidls = oldsum["link"].tolist()
+    if currentsum.empty:
+        currentidls = []
+    else:
+        currentidls = currentsum["link"].tolist()
+    # get current idls not in oldidls
+    newidls = [x for x in currentidls if x not in oldidls]
+
+    if currentsum.empty:
+        newdf = pd.DataFrame()
+    else:
+        newdf = currentsum[currentsum["link"].isin(newidls)]
+    # if newdf is not empty, save it
+    if newdf.empty is False:
+        newdf.reset_index(drop=True, inplace=True)
+        # save to update dtl list
+        toupdname = "pboctoupd" + org_name_index
+        savedf(newdf, toupdname)
+    return newdf
+
+
+# get event detail
+def get_eventdetail(eventsum, orgname):
+    org_name_index = org2name[orgname]
+    browser = get_chrome_driver(temppath)
+    detaills = eventsum["link"].tolist()
+
+    resultls = []
+    errorls = []
+    count = 0
+    for durl in detaills[8:11]:
+        st.info(str(count) + " begin")
+        st.info("url:" + durl)
+        try:
+            browser.get(durl)
+
+            st.write("get download link")
+            # get download link
+            dl1 = browser.find_elements(By.XPATH, '//td[@class="hei14jj"]//a')
+            downurl = []
+            if len(dl1) > 0:
+                for dl in dl1:
+                    dlink = dl.get_attribute("href")
+                    st.write(dlink)
+                    downurl.append(dlink)
+
+            st.write("get table")
+            # get web table
+            dl2 = browser.find_elements(By.XPATH, '//td[@class="hei14jj"]//tr')
+            df = web2table(dl2)
+            st.write(df)
+
+            if len(downurl) > 0:
+                if len(downurl) == 1:
+                    dfl = pd.DataFrame(index=[0])
+                else:
+                    dfl = pd.DataFrame()
+                dfl["link"] = durl
+                dfl["download"] = downurl
+                st.write(dfl)
+                resultls.append(dfl)
+
+            if len(downurl) == 0 and df.empty:
+                dfl = pd.DataFrame(index=[0])
+                dfl["link"] = durl
+                filename = os.path.basename(durl)
+                # unquote to decode url
+                filename = unquote(filename)
+                dfl["file"] = filename
+                st.write(dfl)
+                resultls.append(dfl)
+
+            colen = len(df.columns)
+            if colen == 8:
+                df.columns = [
+                    "序号",
+                    "企业名称",
+                    "处罚决定书文号",
+                    "违法行为类型",
+                    "行政处罚内容",
+                    "作出行政处罚决定机关名称",
+                    "作出行政处罚决定日期",
+                    "备注",
+                ]
+                df["link"] = durl
+                resultls.append(df)
+            #         elif colen==7:
+            #             df['备注']=''
+            #             df.columns=['序号', '企业名称', '处罚决定书文号', '违法行为类型', '行政处罚内容', '作出行政处罚决定机关名称','作出行政处罚决定日期', '备注']
+            #             df['link']=durl
+            #             dwnurl.append(df)
+            #         elif colen==6:
+            #             df['序号']=''
+            #             df['备注']=''
+            #             df.columns=['企业名称', '处罚决定书文号', '违法行为类型', '行政处罚内容', '作出行政处罚决定机关名称','作出行政处罚决定日期', '序号', '备注']
+            #             df['link']=durl
+            #             dwnurl.append(df)
+            elif colen >= 9:
+                df1 = df.drop(df.columns[7], axis=1)
+                df2 = df1[df1.columns[:8]]
+                df2.columns = [
+                    "序号",
+                    "企业名称",
+                    "处罚决定书文号",
+                    "违法行为类型",
+                    "行政处罚内容",
+                    "作出行政处罚决定机关名称",
+                    "作出行政处罚决定日期",
+                    "备注",
+                ]
+                df2["link"] = durl
+                resultls.append(df2)
+            elif colen > 0:
+                st.error("table error" + str(colen))
+                st.error("check" + durl)
+                errorls.append(durl)
+        except Exception as e:
+            st.error("error!: " + str(e))
+            st.error("check url:" + durl)
+            errorls.append(durl)
+
+        mod = (count + 1) % 10
+        if mod == 0 and count > 0:
+            if resultls:
+                tempdf = pd.concat(resultls)
+                savename = "temptodownload-" + org_name_index + str(count + 1)
+                savetemp(tempdf, savename)
+
+        wait = random.randint(2, 20)
+        time.sleep(wait)
+        st.info("finish: " + str(count))
+        count += 1
+
+    # print errorls
+    if len(errorls) > 0:
+        st.error("error list:")
+        st.error(errorls)
+    # if resultls is not empty, save it
+    if resultls:
+        pbocdf = pd.concat(resultls)
+        # get file df
+        filedf = pbocdf[pbocdf["file"].notnull() | pbocdf["download"].notnull()]
+        savecsv = "pboctodownload" + org_name_index
+        # reset index
+        filedf = filedf.reset_index(drop=True)
+        savetemp(filedf, savecsv)
+        # get table df
+        tabledf = pbocdf[pbocdf["file"].isnull() & pbocdf["download"].isnull()]
+        savetable = "pboctotable" + org_name_index + get_now()
+        # reset index
+        tabledf = tabledf.reset_index(drop=True)
+        savetemp(tabledf, savetable)
+    else:
+        pbocdf = pd.DataFrame()
+    return pbocdf
+
+
+def web2table(dl2):
+    tbls = []
+    for tr in dl2:
+        rowls = []
+        dl3 = tr.find_elements(By.TAG_NAME, "td")
+        for td in dl3:
+            #         print(td.text)
+            rowls.append(td.text)
+        tbls.append(rowls)
+    df = pd.DataFrame(tbls)
+    return df
+
+
+def savetemp(df, basename):
+    savename = basename + ".csv"
+    savepath = os.path.join(temppath, savename)
+    df.to_csv(savepath)
+
+
+# download attachment
+def download_attachment(lendf, orgname):
+    org_name_index = org2name[orgname]
+    browser = get_chrome_driver(temppath)
+
+    dwndf = lendf[lendf["download"].notnull()]
+    # get link url
+    linkurl = dwndf["link"]
+    # get download url
+    downloadls = dwndf["download"].tolist()
+
+    resultls = []
+    errorls = []
+    count = 0
+    for link, url in zip(linkurl, downloadls):
+        st.info(str(count) + "begin")
+        st.info("url:" + url)
+        try:
+            browser.get(url)
+            # get filename from url
+            filename = os.path.basename(url)
+            # unquote to decode url
+            filename = unquote(filename)
+            datals = {"link": link, "download": url, "file": filename}
+            df = pd.DataFrame(datals, index=[0])
+            resultls.append(df)
+        except Exception as e:
+            st.error("error!: " + str(e))
+            st.error("check url:" + url)
+            errorls.append(url)
+
+        mod = (count + 1) % 10
+        if mod == 0 and count > 0:
+            tempdf = pd.concat(resultls)
+            savename = "temptofile-" + org_name_index + str(count + 1)
+            savetemp(tempdf, savename)
+
+        wait = random.randint(2, 20)
+        time.sleep(wait)
+        st.info("finish: " + str(count))
+        count += 1
+
+    if resultls:
+        misdf = pd.concat(resultls)
+        filedf = lendf[lendf["download"].isnull()]
+        tabledf = pd.concat([filedf, misdf])
+        savename = "pboctofile" + org_name_index
+        # reset index
+        tabledf.reset_index(drop=True, inplace=True)
+        savetemp(tabledf, savename)
+    else:
+        misdf = pd.DataFrame()
+    return misdf
+
+
+def get_pboctodownload(orgname):
+    org_name_index = org2name[orgname]
+    beginwith = "temp/pboctodownload" + org_name_index
+    pendf = get_csvdf(penpboc, beginwith)
+    return pendf
---
+++ utils.py
@@ -0,0 +1,117 @@
+import glob
+import os
+import datetime
+import pandas as pd
+import streamlit as st
+import streamlit.components.v1 as components
+from pyecharts import options as opts
+from pyecharts.charts import Bar, Grid, Line, Liquid, Page, Pie
+from pyecharts.components import Table
+from pyecharts.options import ComponentTitleOpts
+from st_aggrid import AgGrid
+from st_aggrid.grid_options_builder import GridOptionsBuilder
+from st_aggrid.shared import GridUpdateMode
+from streamlit_echarts import st_pyecharts
+
+rulefolder = "../data/rules"
+
+
+@st.cache
+def get_csvdf(rulefolder):
+    files2 = glob.glob(rulefolder + "**/*.csv", recursive=True)
+    dflist = []
+    for filepath in files2:
+        basename = os.path.basename(filepath)
+        filename = os.path.splitext(basename)[0]
+        newdf = rule2df(filename, filepath)[["监管要求", "结构", "条款"]]
+        dflist.append(newdf)
+    alldf = pd.concat(dflist, axis=0)
+    return alldf
+
+
+def rule2df(filename, filepath):
+    docdf = pd.read_csv(filepath)
+    docdf["监管要求"] = filename
+    return docdf
+
+
+def get_rulefolder(industry_choice):
+    # join folder with industry_choice
+    folder = os.path.join(rulefolder, industry_choice)
+    return folder
+
+
+def df2aggrid(df):
+    gb = GridOptionsBuilder.from_dataframe(df)
+    gb.configure_pagination()
+    # gb.configure_side_bar()
+    # gb.configure_auto_height()
+    gb.configure_default_column(
+        # genablePivot=True,
+        # enableValue=True,
+        # enableRowGroup=True,
+        groupable=True,
+        editable=True,
+    )
+    gb.configure_selection(selection_mode="single", use_checkbox=True)
+    # configure column visibility
+    gb.configure_column(field="lawid", hide=True)
+    gb.configure_column(field="id", hide=True)
+    gridOptions = gb.build()
+    ag_grid = AgGrid(
+        df,
+        theme="material",
+        #  height=800,
+        fit_columns_on_grid_load=True,  # fit columns to grid width
+        gridOptions=gridOptions,  # grid options
+        #  key='select_grid', # key is used to identify the grid
+        update_mode=GridUpdateMode.SELECTION_CHANGED,
+        # data_return_mode=DataReturnMode.FILTERED_AND_SORTED,
+        #  update_mode=GridUpdateMode.NO_UPDATE,
+        # enable_enterprise_modules=True
+    )
+    return ag_grid
+
+
+# split string by space into words, add brackets before and after words, combine into text
+def split_words(text):
+    words = text.split()
+    words = ["(?=.*" + word + ")" for word in words]
+    new = "".join(words)
+    return new
+
+
+# display dataframe in echarts table
+def df2echartstable(df, title):
+    """
+    Display dataframe in echarts table with title
+
+    Parameters:
+    df (pandas.DataFrame): dataframe to display
+    title (str): title of the table
+
+    Example:
+    df2echartstable(df, "Table Title")
+    """
+
+    table = Table()
+    headers = df.columns.tolist()
+    rows = df.values.tolist()
+    table.add(headers, rows)
+    table.set_global_opts(title_opts=opts.ComponentTitleOpts(title=title, subtitle=""))
+    # st_pyecharts(table)
+    components.html(table.render_embed(), width=800)
+
+
+# get current date and time string
+def get_now():
+    now = datetime.datetime.now()
+    now_str = now.strftime("%Y%m%d%H%M%S")
+    return now_str
+
+
+# get current date and time string
+def get_nowdate():
+    now = datetime.datetime.now()
+    now_str = now.strftime("%Y%m%d")
+    return now_str
